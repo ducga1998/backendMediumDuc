8ac3d284da07b74f8b0fdb13112a5fc9bf2de9ab
nguyenminhduc <duc2820@gmail.com>
2019-12-17 16:50:03 +0700

    cas sac

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -9,6 +9,7 @@ if (fs.existsSync(".env")) {
     logger.debug("Using .env.example file to supply config environment variables");
     dotenv.config({ path: ".env.example" });  // you can delete this after you create your own .env file!
 }
+
 export const ENVIRONMENT = process.env.NODE_ENV;
 const prod = ENVIRONMENT === "production"; // Anything else is treated as 'dev'
 
